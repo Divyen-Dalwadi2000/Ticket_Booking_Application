a63f5cccb8547276314a08058561863b50bfefb7
Divyen-Dalwadi2000 <divyen81@gmail.com>
2024-05-01 11:17:00 +0530

    Add a Cinema Model and Design

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -1,9 +1,50 @@
 class CinemasController < ApplicationController
+  
   def index
     @cinemas = Cinema.all
-    
+  end
+
+  def show
+    @cinema = Cinema.find(params[:id])
   end
 
   def new
+    @cinema = Cinema.new
   end
+
+  def create 
+    @cinema = Cinema.create(cinema_params)
+    if @cinema.save
+      redirect_to cinemas_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cinema = Cinema.find(params[:id])
+  end
+
+  def update
+    @cinema = Cinema.find(params[:id])
+    if @cinema.update(cinema_params)
+      redirect_to cinemas_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    p "================================"
+    @cinema = Cinema.find(params[:id])  
+    @cinema.destroy
+    redirect_to cinemas_path
+  end
+
+  private
+
+  def cinema_params
+    params.require(:cinema).permit(:cinema_name , :location , :movie_id )
+  end
+
 end
